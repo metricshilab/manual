1ccc8b843ee904d28abf5767bd398546a7fc9795
Zhentao Shi <shizhentao@gmail.com>
2021-04-29 10:50:49 +0800

    list of useful R packages

---
+++ R/useful_packages.R
@@ -0,0 +1,12 @@
+# some commonly used R packages
+
+install.packages(c(
+    "foreach", "doParallel", "plyr", # parallel
+    "ggplot2", "reshape2", # ggplot2
+    "sandwich", "AER", "mcmc", # econometrics
+    "numDeriv", "optimx", # numerical
+    "tidyverse", "knitr", # tidy
+    "caret", # ML
+    'IRkernel' # IRkernel, much easier than before)
+    )
+)
